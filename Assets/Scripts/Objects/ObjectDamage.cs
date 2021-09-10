5111449f62485abb9653928c0f1fb794db26453c
Ryantheinventor <ryantheinventor1@gmail.com>
2021-09-10 12:59:04 -0700

    Added damage states to crates

---
+++ Assets/Scripts/Objects/ObjectDamage.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectDamage : MonoBehaviour
+{
+    public float speedForDamage = 12f;
+    public string damageStatesFileName = "";
+    private Sprite[] damageStates;
+    private SpriteRenderer sRenderer;
+    private int curState = 0;
+    public float totalV = 0;
+    private void Start()
+    {
+        damageStates = Resources.LoadAll<Sprite>("objects/" + damageStatesFileName);
+        sRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        curState = 0;
+        float rV = totalV += collision.relativeVelocity.magnitude;
+        while (rV > speedForDamage) 
+        {
+            curState++;
+            rV -= speedForDamage;
+        }
+        if (curState > damageStates.Length - 1) 
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            sRenderer.sprite = damageStates[curState];
+        }
+
+    }
+}
